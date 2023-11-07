92f310af1e51ca36392503e2fd1a68adf2c83d18
frechousky <alexandre.freche@gmail.com>
2023-11-07 18:28:58 +0100

    Add LetterBox component

---
+++ src/components/LetterBox.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import LetterBox, { LetterBoxStatus } from './LetterBox'
+import styles from './LetterBox.module.css'
+
+test.each('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''))(
+  'renders %s letter in uppercase',
+  (letter: string) => {
+    render(<LetterBox letter={letter} status={LetterBoxStatus.UNKNOWN} />)
+    const letterHtml = screen.getByText(letter.toUpperCase())
+    expect(letterHtml).toBeInTheDocument()
+  }
+)
+
+test.each([
+  [
+    `${styles.letterBox}  ${styles.goodPosition}`,
+    LetterBoxStatus.GOOD_POSITION
+  ],
+  [`${styles.letterBox}  ${styles.badPosition}`, LetterBoxStatus.BAD_POSITION],
+  [`${styles.letterBox}  ${styles.notPresent}`, LetterBoxStatus.NOT_PRESENT],
+  [`${styles.letterBox}  ${styles.unknown}`, LetterBoxStatus.UNKNOWN]
+])(
+  "has css classes '%s' when status is %s",
+  (expectedClass: string, status: LetterBoxStatus) => {
+    const letter = 'a'
+    render(<LetterBox letter={letter} status={status} />)
+    const letterHtml = screen.getByText(letter.toUpperCase())
+    expect(letterHtml).toHaveClass(expectedClass)
+  }
+)
---
+++ src/components/LetterBox.tsx
@@ -0,0 +1,38 @@
+import styles from './LetterBox.module.css'
+
+export enum LetterBoxStatus {
+  GOOD_POSITION,
+  BAD_POSITION,
+  NOT_PRESENT,
+  UNKNOWN
+}
+
+export type LetterBoxProps = {
+  letter: string
+  status: LetterBoxStatus
+}
+
+function LetterBox (props: LetterBoxProps) {
+  let className = ''
+  switch (props.status) {
+    case LetterBoxStatus.GOOD_POSITION:
+      className = styles.goodPosition
+      break
+    case LetterBoxStatus.BAD_POSITION:
+      className = styles.badPosition
+      break
+    case LetterBoxStatus.NOT_PRESENT:
+      className = styles.notPresent
+      break
+    case LetterBoxStatus.UNKNOWN:
+      className = styles.unknown
+      break
+  }
+  return (
+    <span className={`${styles.letterBox} ${className}`}>
+      {props.letter.toUpperCase()}
+    </span>
+  )
+}
+
+export default LetterBox
